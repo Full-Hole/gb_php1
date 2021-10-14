be18cb00b42cf7fd6c479586c8d10d3880581cee
Timur G <timur91g@gmail.com>
2021-10-14 14:58:39 +0300

    create 3-6.php

--- homework_3/3-6.php
+++ homework_3/3-6.php
@@ -1,16 +1,23 @@
 <?php
-$year = date('Y');
-echo $year;
-$h1 = '<h1>Список Домашних Заданий</h1>';
-$title = 'PHP_Course1';
-$body = '<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>'.$title.'</title>
-</head>
-<body>'.$h1.'<ul>
+/*6. В имеющемся шаблоне сайта заменить статичное меню (ul – li) на генерируемое через PHP.
+ Необходимо представить пункты меню как элементы массива и вывести их циклом.
+  Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать. */
+  function createList(array $listData){
+      $htmlList = "<ul>";
+      foreach($listData as $list => $elem){
+
+
+      }
+      $htmlList .= "</ul>";
+  }
+
+  $dataList =[
+      "Домашняя работа 1" => [],
+      "Домашняя работа 2" => [],
+      "Домашняя работа 3" => [],
+
+  ];
+  /*<ul>
     <li> Домашняя работа 1
         <ul>
             <li><a href="homework_1/1-2.php"> Задание 1-2</a></li>
@@ -30,7 +37,7 @@ $body = '<html lang="en">
             <li><a href="homework_2/2-7.php"> Задание 2-7</a></li>
         </ul>
     </li>
-    <li> Доманняя работа 3
+    <li> Домашняя работа 3
         <ul>
             <li><a href="homework_3/3-1.php"> Задание 3-1</a></li>
             <li><a href="homework_3/3-2.php"> Задание 3-2</a></li>
@@ -40,9 +47,4 @@ $body = '<html lang="en">
             <li><a href="homework_3/3-6.php"> Задание 3-6</a></li>
         </ul>
     </li>
-    </ul>
-<div>
-<span>Кстати сейчас '.$year.' год </span></div>
-</body>
-</html>';
-echo $body;
+    </ul> */
