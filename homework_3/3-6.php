3195899df4f8bf30abd38261d22c817204fe446b
Timur G <timur91g@gmail.com>
2021-10-15 11:31:11 +0300

    add 3-6.php

--- homework_3/3-6.php
+++ homework_3/3-6.php
@@ -4,47 +4,38 @@
   Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать. */
   function createList(array $listData){
       $htmlList = "<ul>";
-      foreach($listData as $list => $elem){
+      foreach($listData as $list => $exercise){
+        $htmlList.="<li> Домашняя работа №$list<ul>";
+            foreach($exercise as $number){
+                $htmlList.="<li><a href='../homework_$list/$list-$number.php'> Задание $list-$number</a></li>";
+            }
 
-
+        $htmlList.="</ul></li>";
       }
       $htmlList .= "</ul>";
+      return $htmlList;
   }
 
   $dataList =[
-      "Домашняя работа 1" => [],
-      "Домашняя работа 2" => [],
-      "Домашняя работа 3" => [],
+      1 => [2,3,4,5],
+      2 => [1,2,3,4,5,6,7],
+      3 => [1,2,3,4,5,6,7,8,9],
 
   ];
-  /*<ul>
-    <li> Домашняя работа 1
-        <ul>
-            <li><a href="homework_1/1-2.php"> Задание 1-2</a></li>
-            <li><a href="homework_1/1-3.php"> Задание 1-3</a></li>
-            <li><a href="homework_1/1-4.php"> Задание 1-4</a></li>
-            <li><a href="homework_1/1-5.php"> Задание 1-5</a></li>
-        </ul>
-    </li>
-    <li> Домашняя работа 2
-        <ul>
-            <li><a href="homework_2/2-1.php"> Задание 2-1</a></li>
-            <li><a href="homework_2/2-2.php"> Задание 2-2</a></li>
-            <li><a href="homework_2/2-3.php"> Задание 2-3</a></li>
-            <li><a href="homework_2/2-4.php"> Задание 2-4</a></li>
-            <li><a href="homework_2/2-5.php"> Задание 2-5</a></li>
-            <li><a href="homework_2/2-6.php"> Задание 2-6</a></li>
-            <li><a href="homework_2/2-7.php"> Задание 2-7</a></li>
-        </ul>
-    </li>
-    <li> Домашняя работа 3
-        <ul>
-            <li><a href="homework_3/3-1.php"> Задание 3-1</a></li>
-            <li><a href="homework_3/3-2.php"> Задание 3-2</a></li>
-            <li><a href="homework_3/3-3.php"> Задание 3-3</a></li>
-            <li><a href="homework_3/3-4.php"> Задание 3-4</a></li>
-            <li><a href="homework_3/3-5.php"> Задание 3-5</a></li>
-            <li><a href="homework_3/3-6.php"> Задание 3-6</a></li>
-        </ul>
-    </li>
-    </ul> */
+  $year = date('Y');
+    $h1 = '<h1>Список Домашних Заданий</h1>';
+    $title = 'PHP_Course1';
+  $body = "<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>$title</title>
+</head>
+<body>$h1".
+createList($dataList)
+."<div>
+<span>Кстати сейчас $year год </span></div>
+</body>
+</html>";
+echo $body;
