97cfbc182bc72532046eca81e0bac9378536f355
Timur G <timur91g@gmail.com>
2021-10-18 17:29:32 +0300

    update main.js, style.css, 4-3.php

--- homework_4/4-3.php
+++ homework_4/4-3.php
@@ -11,9 +11,11 @@ function placeImage(string $path)
 {
     $files = scandir($path);
     //var_dump($files1);
-    foreach ($files as $img) {
-        if (is_file($path . $img)) {
-            echo "<a href=\"$path$img\" target=\"_blank\" class=\"gallery-item\"><img src=\"$path$img\" class=\"gallery-img\"></a>";
+    for($i=0; $i < count($files);$i++){
+    //foreach ($files as $img) {
+        if (is_file($path . $files[$i])) {
+            //echo "<a href=\"$path$img\" target=\"_blank\" class=\"gallery-item\"><img src=\"$path$img\" class=\"gallery-img\"></a>";
+            echo "<img id='img_$i' src=\"$path$files[$i]\" class=\"gallery-img\">";
         }
     }
 }
@@ -23,6 +25,9 @@ function placeImage(string $path)
         <?php placeImage($dir); ?>
     </div>
     <div class="gallery-viewport">
-
+        <div class="vieport-wrapper">
+        <img src="" alt="Bigimg" class="viewport">
+        </div>
     </div>
 </div>
+<script src="../js/main.js"></script>
